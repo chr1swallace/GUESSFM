0eaa32e0da816e6b9014f602aab3b1043f8b4fe8
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2013-12-03 17:02:56 +0000

    added sim data to vignette

--- R/plot.R
+++ R/plot.R
@@ -257,3 +257,87 @@ pp.nsnp <- function(results,plot=FALSE) {
 addlines <- function(summ) {
   geom_vline(data=summ, mapping=aes(xintercept=position.plot,col=tag),alpha=0.5,size=0.2) 
 }
+
+##' Calculate D' and R^2 for a SnpMatrix object and display as a heatmap
+##'
+##' Side effect: displays plot on current graphics device
+##' @title show.ld
+##' @return invisibly returns matrix with D' in upper.tri() entries and R^2 in lower.tri() entries
+##' @author Chris Wallace
+##' @export
+##' @param X a SnpMatrix object
+##' @param snps optional character vector of column names of X (SNPs)
+##' for which LD should be calculated
+##' @param samples optional character vector of row names of X
+##' (Samples) for which LD should be calculated
+##' @param lines.limit draw faint lines to make separation between SNPs clearer if the number of SNPs is < lines.limit
+show.ld <- function(X, snps=colnames(X), samples=rownames(X),
+                    lines.limit=20) {
+  my.colours <- c("#313695", "#4575B4", "#74ADD1", "#ABD9E9", "#E0F3F8", "#FFFFBF",
+                  "#FEE090", "#FDAE61", "#F46D43", "#D73027", "#A50026")
+  if(!is(X,"SnpMatrix"))
+    X <- as(X,"SnpMatrix")
+  breaks <- NULL
+  groups <- NULL
+  if(is.list(snps)) {
+    breaks <- sapply(snps,length)
+    groups <- rep(1:length(snps), times=breaks)
+    snps <- unlist(snps)
+  }
+  LD <- ld(X[samples,snps],
+           depth=length(snps)-1,
+           stat=c("D.prime","R.squared"),
+           symmetric=TRUE)
+  ld <- as.matrix(LD$R.squared)
+  wh <- which(upper.tri(ld))
+  ld[wh] <- as.matrix(LD$D.prime)[ wh ]
+ 
+ diag(ld) <- 1
+ if(!is.null(groups))
+   colnames(ld) <- rownames(ld) <- paste(groups,colnames(ld),sep="/")
+ 
+  df <- melt(as.matrix(ld))
+  df$X1 <- factor(df$X1, levels=colnames(ld))
+  df$X2 <- factor(df$X2, levels=colnames(ld))
+ n <- max(as.numeric(df$X1))
+ p <- ggplot(df) + geom_tile(mapping=aes(x=X1,y=X2,fill=value), linetype=1) +
+    scale_fill_gradientn(colours = my.colours) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1),
+            axis.text = element_text(colour="black"),
+            axis.title = element_blank())
+ if(n<lines.limit) {
+   p <- p + geom_hline(yintercept=(0:n)+0.5, col="grey60") +
+     geom_vline(xintercept=(0:n)+0.5, col="grey60")
+ }
+ 
+if(!is.null(breaks)) {
+  x <- c(0,cumsum(breaks),max(as.numeric(df$X1))) + 0.5
+  n <- length(x)
+  xf <- rbind(data.frame(x=x[-n], y=x[-n], xend=x[-1], yend=x[-n]),
+              data.frame(x=x[-n], y=x[-n], xend=x[-n], yend=x[-1]),
+              data.frame(x=x[-1], y=x[-1], xend=x[-1], yend=x[-n]),
+              data.frame(x=x[-1], y=x[-1], xend=x[-n], yend=x[-1]))
+              
+  show(p + geom_segment(aes(x=x,xend=xend,y=y,yend=yend), data=xf, size=1))
+ } else {
+   show(p)
+ }
+  ## pheatmap(ld,
+  ## cluster_rows=FALSE, cluster_cols=FALSE)
+  invisible(ld)
+}
+## myr2 <- function(X) {
+##   r2 <- ld(X,
+##            depth=ncol(X)-1,
+##            symmetric=TRUE,
+##            stat="R.squared")
+##   if(any(is.na(r2))) {
+##     r2.na <- as(is.na(r2),"matrix")
+##     use <- rowSums(r2.na)>0
+##     ## work around for r2=NA bug.
+##     r2.cor <- as(cor(as(X[,use,drop=FALSE],"numeric"), use="pairwise.complete.obs")^2,"Matrix")
+##     r2[ which(r2.na) ] <- r2.cor[ which(r2.na[use,use]) ]
+##   }
+##   diag(r2) <- 1
+##   return(r2)
+## }
