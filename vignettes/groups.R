4e1e6b4654c0c3751f48ccdc4c48fd20a8514888
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2014-10-13 11:47:28 +0100

    added vignettes

---
+++ vignettes/groups.R
@@ -0,0 +1,17 @@
+### R code from vignette source 'groups.Rnw'
+### Encoding: ASCII
+
+###################################################
+### code chunk number 1: groups.Rnw:47-50
+###################################################
+library(GUESSFM)
+showClass("groups")
+showClass("tags")
+
+
+###################################################
+### code chunk number 2: groups.Rnw:73-74
+###################################################
+showClass("snppicker")
+
+
