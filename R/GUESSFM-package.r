e8c4e942ca2c53f9cfa2c738be038a624042b80d
chr1swallace <chris.wallace@cimr.cam.ac.uk>
2015-03-16 14:14:47 +0000

    dropped dependency on ggbio

--- R/GUESSFM-package.r
+++ R/GUESSFM-package.r
@@ -8,7 +8,6 @@
 #' @importFrom reshape melt melt.data.frame melt.matrix melt.array melt.default
 #' @import ggplot2
 #' @importFrom grid unit
-#' @importFrom ggbio tracks
 #' @importFrom snpStats snp.rhs.tests ld col.summary p.value snp.imputation impute.snps
 #' @importFrom parallel mclapply
 #' @importFrom R2GUESS as.ESS.object
