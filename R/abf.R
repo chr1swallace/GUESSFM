432cb32576902f7951d8ac050a504f518e36797f
Jamie Inshaw <jamie.inshaw@cimr.cam.ac.uk>
2016-06-21 17:16:28 +0100

    Sorted dependency issue from speedglm function

--- R/abf.R
+++ R/abf.R
@@ -316,8 +316,8 @@ abf.speedglm.fit <- function(x,y,q,family,snps,parallel.dir=NULL,verbose=FALSE) 
         cat(i,"\t")
       k0=ncol(qm)
       k=length(snps[[i]]) + k0
-      model <- speedglm.wfit(y2, cbind(x2[, snps[[i]], drop=FALSE ],qm), family=family)
-      model0 <- speedglm.wfit(y2, qm, family=family)
+      model <- speedglm.wfit1(y2, cbind(x2[, snps[[i]], drop=FALSE ],qm), family=family)
+      model0 <- speedglm.wfit1(y2, qm, family=family)
 #      class(model) <- class(model0) <- c(class(model),"glm")
       A1 <- AIC(model) - 2*k + k*log(length(y2))
       A0 <- AIC(model0) - 2*k0 + k0*log(length(y2))
@@ -482,3 +482,134 @@ abf.glm <- function(x,y,q,family,snps,verbose=FALSE) {
 
 pdata <- function(d) file.path(d,"data.RData")
 presults <- function(d) file.path(d,"results.RData")
+
+
+##' Internal adaption of speedglm function speedglm.wfit1 due to dependency issue.  See speeglm package for thorough documentation.
+##'
+##' @title speedglm.wfit1
+##' @author Jamie Inshaw
+##' @export
+
+speedglm.wfit1 <- function(y, X, intercept=TRUE, weights=NULL,row.chunk=NULL,
+                           family=gaussian(), start=NULL, etastart=NULL,
+                           mustart=NULL, offset=NULL, acc=1e-08, maxit=25, k=2,
+                           sparselim=.9,camp=.01, eigendec=TRUE, tol.values=1e-7,
+                           tol.vectors=1e-7, tol.solve=.Machine$double.eps,
+                           sparse=NULL,method = c('eigen','Cholesky','qr'), 
+                           trace=FALSE,...){
+  nobs <- NROW(y)
+  nvar <- ncol(X) 
+  if (missing(y)) stop("Argument y is missing")
+  if (missing(X)) stop("Argument X is missing") 
+  if (is.null(offset)) offset <- rep.int(0, nobs)  
+  if (is.null(weights)) weights <- rep(1, nobs)  
+  col.names <- dimnames(X)[[2]]
+  method <- match.arg(method)
+  fam  <- family$family
+  link <- family$link  
+  variance <- family$variance
+  dev.resids <- family$dev.resids
+  aic <- family$aic
+  linkinv <- family$linkinv
+  mu.eta <- family$mu.eta 
+  if (is.null(sparse)) sparse <- is.sparse(X=X,sparselim,camp) 
+  if (is.null(start)) {
+    if (is.null(mustart)) eval(family$initialize)
+    eta <- if (is.null(etastart)) family$linkfun(mustart) else etastart
+    mu <- mustart
+    start <- rep(0,nvar)
+  } else { 
+    eta <- offset + as.vector(if (nvar == 1) X*start else {
+      if (sparse) X%*%start else tcrossprod(X,t(start))})
+    mu <- linkinv(eta)
+  }  
+  iter <- 0
+  dev <- sum(dev.resids(y, mu, weights)) 
+  tol <- 1
+  if ((fam=="gaussian")&(link=="identity")) maxit <- 1
+  C_Cdqrls <- getNativeSymbolInfo('Cdqrls', PACKAGE=getLoadedDLLs()$stats)
+  while((tol>acc)&(iter<maxit)){
+    iter <- iter + 1
+    beta <- start
+    dev0 <- dev
+    varmu <- variance(mu)
+    mu.eta.val <- mu.eta(eta)    
+    z <- (eta - offset) + (y - mu)/mu.eta.val   
+    z <- as.matrix(z) 
+    W <- (weights*mu.eta.val*mu.eta.val)/varmu 
+    XTX <- cp(X,W,row.chunk,sparse)
+    XTz <- if (sparse) t(X)%*%(W*z) else t(crossprod((W*z),X))    
+    if (iter==1 & method != 'qr') {
+      variable <- colnames(X) 
+      ris <- if (eigendec) control(XTX,,tol.values,tol.vectors,,method) else 
+        list("rank"= nvar,"pivot"=1:nvar)               
+      ok <- ris$pivot[1:ris$rank]
+      if (eigendec) {
+        XTX <- ris$XTX
+        X <- X[,ok]
+        XTz<-XTz[ok]
+        start <- start[ok]
+      }     
+      beta <- start           
+    }  
+    if(method=='qr') {
+      ris<-.Call(C_Cdqrls,XTX, XTz, tol.values, FALSE)
+      start <- if (ris$rank < nvar) ris$coefficients[ris$pivot]
+      else ris$coefficients
+    } else {
+      start <- solve(XTX,XTz,tol=tol.solve)
+    }
+    eta <- if (sparse) drop(X%*%start) else drop(tcrossprod(X,t(start)))
+    mu <- linkinv(eta <- eta + offset)
+    dev <- sum(dev.resids(y, mu, weights)) 
+    tol <- max(abs(dev0-dev)/(abs(dev)+0.1))
+    if (trace) cat("iter",iter,"tol",tol,"\n")
+  } 
+  wt <- sum(weights)
+  wtdmu <- if (intercept) sum(weights * y)/wt  else linkinv(offset)
+  nulldev <- sum(dev.resids(y, wtdmu, weights))
+  n.ok <- nobs - sum(weights == 0)
+  nulldf <- n.ok - as.integer(intercept) 
+  rank <- ris$rank
+  dfr <- nobs - rank - sum(weights == 0)
+  aic.model <- aic(y, nobs, mu, weights, dev) + k * rank
+  ll.nuovo <- ll.speedglm1(fam,aic.model,rank)
+  res <- (y-mu)/mu.eta(eta)
+  resdf <- n.ok - rank
+  RSS <- sum(W*res*res)
+  var_res <- RSS/dfr  
+  dispersion <- if (fam %in% c("poisson","binomial")) 1 else var_res   
+  if(method=='qr'){
+    coefficients<-start
+    coefficients[coefficients==0]=NA
+    ok <- ris$pivot[1:rank]
+  } 
+  else {
+    coefficients <- rep(NA,nvar)
+    start<- as(start,"numeric")
+    coefficients[ok] <- start
+  }
+  names(coefficients) <- col.names
+  rval <- list("coefficients"=coefficients,"logLik"=ll.nuovo,"iter"=iter,
+               "tol"= tol,"family"=family,"link"=link,"df"=dfr,"XTX"=XTX,
+               "dispersion"=dispersion,"ok"=ok,"rank"=rank,"RSS"=RSS, method=method,
+               "aic"=aic.model, "sparse"=sparse,"deviance"=dev,"nulldf"=nulldf, 
+               "nulldev"=nulldev,"ngoodobs"=n.ok,"n"=nobs,"intercept"=intercept,
+               "convergence"=(!(tol>acc)))   
+  class(rval) <- "speedglm"
+  return(rval)
+}
+
+
+
+ll.speedglm1 <- function(family,aic.model,nvar){
+  switch(family,
+         binomial = -(aic.model-2 * nvar)/2,
+         Gamma = -((aic.model-2 * nvar)-2)/2,
+         gaussian = -((aic.model-2 * nvar)-2)/2,
+         poisson = -(aic.model- 2 * nvar)/2,
+         inverse.gaussian = -((aic.model-2 * nvar)-2)/2,
+         quasi = NA,
+         quasibinomial = NA,
+         quasipoisson = NA)
+}
