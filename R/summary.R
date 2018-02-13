c74404f2e9dd1fad0e0b1f4f0118c47f7cae06af
Jamie Inshaw <jamie.inshaw@cimr.cam.ac.uk>
2018-02-13 14:15:02 +0000

    removed the error

--- R/summary.R
+++ R/summary.R
@@ -195,7 +195,7 @@ best.models <- function(d,pp.thr=0.01,cpp.thr=NA) {
     return(lapply(d,best.models,pp.thr=pp.thr,cpp.thr=cpp.thr))
   if(!is(d,"snpmod"))
     stop("expected d to be a snpmod object, found ",class(d))
-  o <- order(d@models$PP,decreasing=TRUE),
+  o <- order(d@models$PP,decreasing=TRUE)
   d@models <- d@models[ o ] # just in case
   d@model.snps <- d@model.snps[ o ] # just in case
   if(!is.na(cpp.thr)) {
